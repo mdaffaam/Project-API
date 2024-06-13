e9136b382d544d89a36e1196a4a56976854147c2
Muhammad Daffa Abiyyu Muhana <116410520+mdaffaam@users.noreply.github.com>
2024-06-13 18:49:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.use(AuthRoute);
 
 // store.sync();
 
-app.listen(process.env.APP_PORT, ()=> {
-    console.log('Server up and running...');
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server up and running on port ${PORT}...`);
 });
