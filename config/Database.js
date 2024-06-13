1522f4ea7f2fcf63a8f4aa020e6650437ca8a002
Muhammad Daffa Abiyyu Muhana <116410520+mdaffaam@users.noreply.github.com>
2024-06-13 14:58:12 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,15 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
+// Informasi koneksi Cloud SQL
+const db = new Sequelize('auth_db', 'root', 'the77gameplay', {
+    host: "34.101.163.152",
+    dialect: "mysql",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default db;
